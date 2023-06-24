97051866d2991b7a91c7f13c1e59f6e82ae24973
mnhii <123044370+mnhii@users.noreply.github.com>
2023-06-24 20:42:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -323,7 +323,7 @@ if st.session_state.clicked:
 #     """,
 #     unsafe_allow_html=True
 # )
-        d, penetration = st.columns([4,26])
+        d, penetration = st.columns([4,25])
         with d:
             d = st.date_input(label='Today', value=date.today())
             d = datetime.combine(d, datetime.min.time())
