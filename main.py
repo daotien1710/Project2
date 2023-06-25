21c3d35ac1a40f069f7c97dae6b26761ec496946
mnhii <123044370+mnhii@users.noreply.github.com>
2023-06-25 14:17:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -362,7 +362,7 @@ background-attachment: local;
         with penetration:
             st.write()
 
-        col1, col2= st.columns([0.59, 0.41], gap="small")
+        col1, col2= st.columns([0.8, 0.2], gap="small")
 
         # print(df_one_year)
         
