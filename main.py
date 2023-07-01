c5701ce5e81b797f05ed9ffdecc28cb6dadc313f
mnhii <mynhi2303@gmail.com>
2023-07-02 00:26:09 +0700

    rgpo

--- main.py
+++ main.py
@@ -494,7 +494,7 @@ background-attachment: local;
     # """ Add Content Tab Gender """
     with tab2:
         st.markdown('<span style="font-family: SVN-Gilroy; font-size: 32px; font-weight: bold;">CUSTOMER TREND</span>', unsafe_allow_html=True)
-        col1, col2= st.columns([0.55, 0.45], gap="small")
+        col1, col2= st.columns([0.6, 0.4], gap="small")
         from plotly import graph_objects as go
         with col1:
             fig = go.Figure(go.Funnel(
