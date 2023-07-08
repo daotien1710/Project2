da853cdb91a8ab61b9272c9dc5864faa4b6a9017
mnhii <mynhi2303@gmail.com>
2023-07-08 23:12:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -279,6 +279,7 @@ if st.session_state.clicked:
         selectedGender = [ele[0] for ele in selectedGender.values.tolist() if ele[1]]
         if 'All' in selectedGender or len(selectedGender) == 0: selectedGender = df['Gender'].unique()
         df = df[df['Gender'].isin(selectedGender)]
+        df.reset_index(inplace=True, drop=True)
         
         """ Select Nationality """
         for c in ['Nationality']:
