aa9238bab8c55250a94945bffb8e8d2681a5087b
mnhii <mynhi2303@gmail.com>
2023-07-09 00:23:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -537,7 +537,7 @@ if st.session_state.clicked:
                               font=dict(family="SVN-Gilroy",size=14),
                               paper_bgcolor='rgba(255, 255, 255, 0)',
                               plot_bgcolor='rgba(255, 255, 255, 0.1)',
-                              width=500, height=500)
+                              width=450, height=450)
             fig.update_traces(showlegend=False)
             st.plotly_chart(fig)
             st.markdown('<span style="font-family: SVN-Gilroy; font-size: 20px; font-weight: bold;">BOXPLOT ILLUSTRATES THE PRICE OF ACCOMMODATIONS</span>', unsafe_allow_html=True)
@@ -549,7 +549,7 @@ if st.session_state.clicked:
                               yaxis=dict(title="<b>COST OF TRANSPORTATION</b>",title_font=dict(family="SVN-Gilroy",size=14)),
                               legend=dict(title="GENDER",title_font=dict(family="SVN-Gilroy", size=14),font=dict(family="SVN-Gilroy",size=14)),
                               font=dict(family="SVN-Gilroy",size=14),paper_bgcolor='rgba(255, 255, 255, 0)',plot_bgcolor='rgba(255, 255, 255, 0.1)',
-                              width=500, height=500)
+                              width=450, height=450)
             fig.update_traces(showlegend=False)
             st.plotly_chart(fig)
             st.markdown('<span style="font-family: SVN-Gilroy; font-size: 20px; font-weight: bold;">BOXPLOT ILLUSTRATES THE PRICE OF TRANSPORTATIONS</span>', unsafe_allow_html=True)
