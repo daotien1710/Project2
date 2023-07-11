c0196880fc2104cf21943b6d97490fce223fefd0
mnhii <mynhi2303@gmail.com>
2023-07-11 10:10:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -253,8 +253,8 @@ if st.session_state.clicked:
 <style>
 [data-testid="stAppViewContainer"] > .main {{
 background-image: url("data:image/png;base64,{img2}");
-background-size: 500%
-background-position: top-right;
+background-size: 500%;
+background-position: right;
 background-repeat: no-repeat;
 background-attachment: local;
 }}
