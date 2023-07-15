721fd9b60d01ed89d53a25847e3a426baecdb4ba
mnhii <mynhi2303@gmail.com>
2023-07-16 01:04:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -370,8 +370,8 @@ background-attachment: local;
         st.session_state.Continent = []
         st.session_state.AccommodationType = []
         st.session_state.TransportationType = []
-        st.session_state.Month = []
-        st.session_state.Year = []
+        st.session_state.Month = month
+        st.session_state.Year = year
         # st.session_state.Month = m[date.today().month]
         # st.session_state.Year = date.today().year
 
