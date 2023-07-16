fe2d7bc79adf511c8758deecee234cca830e107a
mnhii <mynhi2303@gmail.com>
2023-07-16 21:38:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -505,7 +505,7 @@ background-attachment: local;
             st.write('**The graph above displays both Revenue and LTM Revenue the company obtained between August 2022 and July 2023, a period of one full year.**')
 
         with col2:
-            threshold=3600
+            threshold=3000
             r = df.groupby('DepartureCountry').sum()['Revenue'].to_frame().reset_index()
             f = r[r['Revenue'] >= threshold]
             f = f.append({'DepartureCountry': 'Other',
